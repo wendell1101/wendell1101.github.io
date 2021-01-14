d72e6ac0b57f47d06e4697e30b24d2f7bfedc860
Wendell Suazo <wendellchansuazo11@gmail.com>
2021-01-14 18:10:50 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//Variables
+const hamburger = document.querySelector('.hamburger');
+const bar1 = document.querySelector('.bar1');
+const bar2 = document.querySelector('.bar2');
+const bar3 = document.querySelector('.bar3');
+const sideNav = document.querySelector('.side-nav');
+const wrapper = document.querySelector('.wrapper');
+const navbar = document.querySelector('.main-nav')
+
+// hamburger menu side nav
+hamburger.addEventListener('click', () => {
+    // nav.classList.toggle('main-nav-active');
+    hamburger.classList.toggle('hamburger-active');
+    bar1.classList.toggle('bar1-cross-left');
+    bar2.classList.toggle('bar2-active');
+    bar3.classList.toggle('bar3-cross-right');
+    sideNav.classList.toggle('active');
+    wrapper.classList.toggle('active');
+})
+
+// fixed navbar
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > navbar.offsetHeight + 200) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+}
+
+// Initialize AOS
+AOS.init();
+
+// Play project GIF on mouse over
+
+const ecommerceProject = document.getElementById('ecommerce-project');
+
+ecommerceProject.addEventListener('mouseover', () => {
+    ecommerceProject.src = './img/enrico_merchandise2.gif';
+})
+ecommerceProject.addEventListener('mouseout', () => {
+    ecommerceProject.src = './img//enrico_merchandise_thumbnail.jpg';
+})
