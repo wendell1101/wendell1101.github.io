cb526f08b768bb6bb74e596a577fbb05926ab44f
Wendell Suazo <wendellchansuazo11@gmail.com>
2021-01-26 17:55:24 +0800

    add active links and resume plus back to top button

--- js/main.js
+++ js/main.js
@@ -29,8 +29,57 @@ function fixNav() {
     }
 }
 
+//main-nav active links
+const navLinks = document.getElementsByClassName("nav-link");;
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+    });
+}
+
+//side-nav active links
+const sideLinks = document.getElementsByClassName("side-link");;
+
+for (let i = 0; i < sideLinks.length; i++) {
+    sideLinks[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("side-link active");
+
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+    });
+}
+
 // Initialize AOS
 AOS.init();
 
-// Play project GIF on mouse over
 
+//jquery
+// back to top button
+let btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
